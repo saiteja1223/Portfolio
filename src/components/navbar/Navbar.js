858a068988ce3993b6db109b940fe1cfbdd56285
sai <dodda.saiteja123@gmail.com>
2024-01-12 21:02:46 +0530

    Your commit message hee

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -22,11 +22,11 @@ const Navbar = () => {
         <img src='./Asserts/menu.png' className='mobMenu' alt='' onClick={()=>setShowMenu(!showMenu)}/>
     
     <div className='navMenu' style={{display:showMenu?'flex':'none'}} >
-        <Link activeClass="active" to='intro' spy={true} smooth={true} offset={-100} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
-        <Link activeClass="active" to='skills' spy={true} smooth={true} offset={15} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>About</Link> 
-        <Link activeClass="active" to='works' spy={true} smooth={true} offset={10} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
-        <Link activeClass="active" to='clients' spy={true} smooth={true} offset={10} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Client</Link>
-        <Link activeClass="active" to='contact' spy={true} smooth={true} offset={-50} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>contact</Link>
+        <Link activeClass="active" to='intro' spy={true} smooth={true} offset={-60} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
+        <Link activeClass="active" to='skills' spy={true} smooth={true} offset={-60} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>About</Link> 
+        <Link activeClass="active" to='works' spy={true} smooth={true} offset={-60} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
+        <Link activeClass="active" to='clients' spy={true} smooth={true} offset={-60} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>Client</Link>
+        <Link activeClass="active" to='contact' spy={true} smooth={true} offset={-60} duration={500} className="listItem" onClick={()=>setShowMenu(false)}>contact</Link>
         <button className='dexktopMenuBtn' onClick={()=>document.getElementById('skills').scrollIntoView({behavior:'smooth'})}>info</button>
     </div>
     
